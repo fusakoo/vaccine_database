565b81179b00d408bf9a4af465a6e878a1b8ecdc
exleyt <exleyt@oregonstate.edu>
2021-10-27 03:15:27 -0700

    Doses In People Form Submit functionality

--- src/components/DoesesInPeople/DoesesInPeopleForm.js
+++ src/components/DoesesInPeople/DoesesInPeopleForm.js
@@ -1,25 +1,69 @@
 import React from 'react';
 
-function DoesesInPeopleForm() {
-  return (
-      <form className="new-input">
+function Validate(id) { 
+
+  const errors = [];
+
+  if (id > -1) { 
+    errors.push("id must be non-negative");
+  }
+
+  return errors;
+}
+class DoesesInPeopleForm extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      id: -1,
+      errors: []
+    };
+  }
+
+  HandleSubmit = (e) => {
+    e.preventDefault();
+
+    const { id } = this.state;
+
+    // DO SQL INSERT
+
+    const errors = Validate(id);
+    const hasErrors = errors.length > 0;
+    if (hasErrors) { 
+      this.setState({ errors });
+      return;
+    }
+
+  };
+
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <form className="new-input" onSubmit={this.HandleSubmit}>
+        {errors.map(error => (
+          <p key={error}>Error: {error}</p>
+        ))}
         <p><span className="required">* </span><span className="optional">Is required</span></p>
         <div class="form-group">
-            <label>id <span className="required">*</span>
-              <input
-                  type="number"
-                  name="id"
-                  placeholder="Person ID"
-                  className="form-control"
-                  required
-              />
-            </label>
+          <label>id <span className="required">*</span>
+            <input
+              value={this.state.id}
+              onChange={e => this.setState({ id: e.target.value })}
+              type="number"
+              name="id"
+              placeholder="Person ID"
+              className="form-control"
+              required
+            />
+          </label>
         </div>
         <div class="form-group">
-            <button type="submit" id="submit" className="submit-button">Select Person</button>
+          <button type="submit" id="submit" className="submit-button">Select Person</button>
         </div> 
       </form>
-  );
+    );
+  }
 }
 
 export default DoesesInPeopleForm;
