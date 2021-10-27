c8126e5db47ff95d4daaa95ffaa18fe6ff5600ee
exleyt <exleyt@oregonstate.edu>
2021-10-26 18:02:04 -0700

    Corrected mRNA vaccine type

--- src/data/vaccines.js
+++ src/data/vaccines.js
@@ -1,7 +1,7 @@
 // The research_name of vaccines will be unique
 const vaccines = [
-  { research_name: 'BNT162b2', manufacturer: 'Pfizer', vaccine_type: 'mNRA' },
-  { research_name: 'mRNA-1273', manufacturer: 'Moderna', vaccine_type: 'mNRA' },
+  { research_name: 'BNT162b2', manufacturer: 'Pfizer', vaccine_type: 'mRNA' },
+  { research_name: 'mRNA-1273', manufacturer: 'Moderna', vaccine_type: 'mRNA' },
   { research_name: 'JNJ-78436735', manufacturer: 'Janssen Pharmaceutical Companies', vaccine_type: 'viral vector' }
 ];
 
