9ea2cd49309477deb850fd13486913fb29ec9eaf
fusakoo <obatafusako@gmail.com>
2021-10-27 00:14:07 -0700

    Get the radio button selector for people form working

--- src/components/People/PeopleForm.js
+++ src/components/People/PeopleForm.js
@@ -1,56 +1,137 @@
 import React from 'react';
 
 function PeopleForm() {
+  const [status, setStatus] = React.useState(0) // 0: show new form, 1: show update form
+
+  const radioHandler = (status) => {
+    setStatus(status);
+  };
+
   return (
-      <form className="new-input">
-        <p><span className="required">* </span><span className="optional">Is required</span></p>
-        <div class="form-group">
-            <label>last_name <span className="required">*</span>
-              <input
-                  type="text"
-                  name="last_name"
-                  placeholder="Last name"
-                  className="form-control"
-                  required
-              />
-            </label>
-        </div>
-        <div class="form-group">
-            <label>first_name <span className="required">*</span>
-              <input
-                  type="text"
-                  name="first_name"
-                  placeholder="First name"
-                  className="form-control"
-                  required
-              />
-            </label>
-        </div>
-        <div class="form-group">
-            <label>birth_date <span className="required">*</span>
-              <input
-                  type="text"
-                  name="birth_date"
-                  placeholder="YYYY-MM-DD"
-                  className="form-control"
-                  required
-              />
-            </label>
-        </div>
-        <div class="form-group">
-            <label>site_id <span className="optional">(Optional)</span>
-              <input
-                  type="number"
-                  name="site_id"
-                  placeholder="Site ID"
-                  className="form-control"
-              />
-            </label>
-        </div>       
-        <div class="form-group">
-            <button type="submit" id="submit" className="submit-button">Add New</button>
-        </div> 
-      </form>
+      <>
+        <form className="new-input">
+          <div className="form-group">
+            <input 
+                // value="new" 
+                type="radio" 
+                name="formselector" 
+                className="input-radio"
+                checked={status === 0}
+                onClick={(e) => radioHandler(0)}
+            />Add New Entry
+            <br/>
+            <input 
+                // value="update" 
+                type="radio" 
+                name="formselector" 
+                className="input-radio"
+                checked={status === 1}
+                onClick={(e) => radioHandler(1)}
+            />Edit Existing
+          </div>
+          <p><span className="required">* </span><span className="optional">Is required</span></p>
+          {status === 0 && (
+            <>
+              <div className="form-group">
+                <label>last_name <span className="required">*</span>
+                  <input
+                      type="text"
+                      name="last_name"
+                      placeholder="Last name"
+                      className="form-control"
+                      required
+                  />
+                </label>
+              </div>
+              <div className="form-group">
+                  <label>first_name <span className="required">*</span>
+                    <input
+                        type="text"
+                        name="first_name"
+                        placeholder="First name"
+                        className="form-control"
+                        required
+                    />
+                  </label>
+              </div>
+              <div className="form-group">
+                  <label>birth_date <span className="required">*</span>
+                    <input
+                        type="text"
+                        name="birth_date"
+                        placeholder="YYYY-MM-DD"
+                        className="form-control"
+                        required
+                    />
+                  </label>
+              </div>
+              <div className="form-group">
+                  <label>site_id <span className="optional">(Optional)</span>
+                    <input
+                        type="number"
+                        name="site_id"
+                        placeholder="Site ID"
+                        className="form-control"
+                    />
+                  </label>
+              </div>
+              <div className="form-group">
+                  <button type="submit" id="submit" className="submit-button">Add New</button>
+              </div> 
+            </>
+          )}
+          {status === 1 && (
+            <>
+              <div className="form-group">
+                  <label>id <span className="required">*</span>
+                    <input
+                        type="number"
+                        name="id"
+                        placeholder="Person's ID"
+                        className="form-control"
+                        required
+                    />
+                  </label>
+              </div>
+              <div className="form-group">
+                <label>last_name <span className="optional">(Optional)</span>
+                  <input
+                      type="text"
+                      name="last_name"
+                      placeholder="Last name"
+                      className="form-control"
+                      required
+                  />
+                </label>
+              </div>
+              <div className="form-group">
+                  <label>first_name <span className="optional">(Optional)</span>
+                    <input
+                        type="text"
+                        name="first_name"
+                        placeholder="First name"
+                        className="form-control"
+                        required
+                    />
+                  </label>
+              </div>
+              <div className="form-group">
+                  <label>site_id <span className="optional">(Optional)</span>
+                    <input
+                        type="number"
+                        name="site_id"
+                        placeholder="Site ID"
+                        className="form-control"
+                    />
+                  </label>
+              </div>
+              <div className="form-group">
+                <button type="submit" id="submit" className="submit-button">Update</button>
+              </div> 
+            </>
+          )} 
+        </form>
+      </>
   );
 }
 
---
+++ src/components/People/PeopleFormOld.js
@@ -0,0 +1,111 @@
+// Keeping this in case we want to change the looks (2 different form version)
+
+// import React from 'react';
+
+// function PeopleForm() {
+//   return (
+//       <>
+//         <form className="new-input">
+//           <h2>Add New</h2>
+//           <p><span className="required">* </span><span className="optional">Is required</span></p>
+//           <div class="form-group">
+//               <label>last_name <span className="required">*</span>
+//                 <input
+//                     type="text"
+//                     name="last_name"
+//                     placeholder="Last name"
+//                     className="form-control"
+//                     required
+//                 />
+//               </label>
+//           </div>
+//           <div class="form-group">
+//               <label>first_name <span className="required">*</span>
+//                 <input
+//                     type="text"
+//                     name="first_name"
+//                     placeholder="First name"
+//                     className="form-control"
+//                     required
+//                 />
+//               </label>
+//           </div>
+//           <div class="form-group">
+//               <label>birth_date <span className="required">*</span>
+//                 <input
+//                     type="text"
+//                     name="birth_date"
+//                     placeholder="YYYY-MM-DD"
+//                     className="form-control"
+//                     required
+//                 />
+//               </label>
+//           </div>
+//           <div class="form-group">
+//               <label>site_id <span className="optional">(Optional)</span>
+//                 <input
+//                     type="number"
+//                     name="site_id"
+//                     placeholder="Site ID"
+//                     className="form-control"
+//                 />
+//               </label>
+//           </div>
+//           <div class="form-group">
+//               <button type="submit" id="submit" className="submit-button">Submit</button>
+//           </div> 
+//         </form>
+//         <form className="new-input">
+//           <h2>Update</h2>
+//           <div class="form-group">
+//               <label>last_name <span className="optional">(Optional)</span>
+//                 <input
+//                     type="text"
+//                     name="last_name"
+//                     placeholder="Last name"
+//                     className="form-control"
+//                     required
+//                 />
+//               </label>
+//           </div>
+//           <div class="form-group">
+//               <label>first_name <span className="optional">(Optional)</span>
+//                 <input
+//                     type="text"
+//                     name="first_name"
+//                     placeholder="First name"
+//                     className="form-control"
+//                     required
+//                 />
+//               </label>
+//           </div>
+//           {/* <div class="form-group">
+//               <label>birth_date <span className="required">*</span>
+//                 <input
+//                     type="text"
+//                     name="birth_date"
+//                     placeholder="YYYY-MM-DD"
+//                     className="form-control"
+//                     required
+//                 />
+//               </label>
+//           </div> */}
+//           <div class="form-group">
+//               <label>site_id <span className="optional">(Optional)</span>
+//                 <input
+//                     type="number"
+//                     name="site_id"
+//                     placeholder="Site ID"
+//                     className="form-control"
+//                 />
+//               </label>
+//           </div>
+//           <div class="form-group">
+//               <button type="submit" id="submit" className="submit-button">Update</button>
+//           </div> 
+//         </form>
+//       </>
+//   );
+// }
+
+// export default PeopleForm;
