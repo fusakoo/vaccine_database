690106b80e1ecb5e7d3670adb69c1420340af53a
exleyt <exleyt@oregonstate.edu>
2021-10-27 03:07:14 -0700

    Vaccines Form Submit functionality

--- src/components/Vaccines/VaccinesForm.js
+++ src/components/Vaccines/VaccinesForm.js
@@ -1,12 +1,54 @@
 import React from 'react';
 
-function VaccinesForm() {
-  return (
-      <form className="new-input">
+function Validate() { 
+
+  const errors = [];
+
+  return errors;
+}
+
+class VaccinesForm extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      research_name: "",
+      manufacturer: "",
+      vaccine_type: "",
+      errors: []
+    };
+  }
+
+  HandleSubmit = (e) => {
+    e.preventDefault();
+
+    const { research_name, manufacturer, vaccine_type } = this.state;
+
+    // DO SQL INSERT
+
+    const errors = Validate();
+    const hasErrors = errors.length > 0;
+    if (hasErrors) { 
+      this.setState({ errors });
+      return;
+    }
+
+  };
+
+  render() {
+    const { errors } = this.state;
+
+    return (
+        <form className="new-input" onSubmit={this.HandleSubmit}>
+          {errors.map(error => (
+            <p key={error}>Error: {error}</p>
+          ))}
         <p><span className="required">* </span><span className="optional">Is required</span></p>
         <div class="form-group">
             <label>research_name <span className="required">*</span>
               <input
+                  value={this.state.research_name}
+                  onChange={e => this.setState({ research_name: e.target.value })}
                   type="text"
                   name="research_name"
                   placeholder="Research name"
@@ -18,6 +60,8 @@ function VaccinesForm() {
         <div class="form-group">
             <label>manufacturer <span className="required">*</span>
               <input
+                  value={this.state.manufacturer}
+                  onChange={e => this.setState({ manufacturer: e.target.value })}
                   type="text"
                   name="manufacturer"
                   placeholder="Manufacturer name"
@@ -29,6 +73,8 @@ function VaccinesForm() {
         <div class="form-group">
             <label>manufacturer <span className="required">*</span>
               <input
+              value={this.state.vaccine_type}
+              onChange={e => this.setState({ vaccine_type: e.target.value })}
                   type="text"
                   name="vaccine_type"
                   placeholder="Vaccine type"
@@ -41,7 +87,8 @@ function VaccinesForm() {
             <button type="submit" id="submit" className="submit-button">Add New</button>
         </div> 
       </form>
-  );
+    );
+  }
 }
 
 export default VaccinesForm;
