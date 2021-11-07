48f977183ef4ac64c5c02cdfae475fe491019396
Fusako Obata <obataf@flip1.engr.oregonstate.edu>
2021-11-06 23:55:05 -0700

    Fix the static data typos

--- src/data/doses.js
+++ src/data/doses.js
@@ -1,7 +1,7 @@
 // Dummy relationship table to represent doses table
 const doses = [
   { id: 1, research_name: 'BNT162b2', date_taken: '2021-10-01'},
-  { id: 1, research_name: 'mRNA-1273', date_taken: '2021-10-15'},
+  { id: 1, research_name: 'BNT162b2', date_taken: '2021-10-15'},
   { id: 2, research_name: 'mRNA-1273', date_taken: '2021-05-16'}
 ];
 
